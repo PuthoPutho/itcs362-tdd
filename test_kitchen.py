a8d19e3966592886c0016ed3ce54f3b70f0928b0
PuthoPutho <cthananchanok@gmail.com>
2026-09-03 12:11:34 +0700

    implement A5 ,it fail 1 case

--- test_kitchen.py
+++ test_kitchen.py
@@ -35,4 +35,8 @@ def test_equality():
     assert Quantity(200) == Quantity(200)
     assert Quantity(200) != Quantity(300)
 
+#A5
+def test_grams_are_not_ounces():
+    assert Quantity(1, "g") != Quantity(1, "oz")
+
 
