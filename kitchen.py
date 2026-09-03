d5886ecc0adb52862aa34a0b7ed3147cb2f5380d
PuthoPutho <cthananchanok@gmail.com>
2026-09-03 12:07:18 +0700

    implement A3 (6787042)

--- kitchen.py
+++ kitchen.py
@@ -7,7 +7,8 @@ class Quantity:
     #    self.amount = 600
     
     def times(self, multiplier):
-        self.amount = self.amount * multiplier
+        return Quantity(self.amount * multiplier)
+
  
 
         
